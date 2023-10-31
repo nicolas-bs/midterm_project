b95516e0feb6a4a2050ff3137f217f9f5af33b27
nicolas-bs <69317512+nicolas-bs@users.noreply.github.com>
2023-10-31 13:01:24 -0500

    Added the support of make multiple predictions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pickle
 import pandas as pd
-from fastapi import FastAPI, HTTPException
-import xgboost as xgb
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from pydantic import BaseModel
+import xgboost as xgb
+from fastapi.responses import JSONResponse
+import io  # Add this import
 
 app = FastAPI()
 
@@ -29,23 +31,49 @@ class InputData(BaseModel):
     change: str
     diabetes_med: str
 
-@app.post('/predict')
+@app.post('/Individual')
 async def predict(data: InputData):
-
-    input_data = [data.dict()]
+    input_data = [data.model_dump()]
     input_df = pd.DataFrame(input_data)
-
     X = preprocessor.transform(input_df)
     feature_names = preprocessor.get_feature_names_out()
     feature_names = [name.replace('[', '_').replace(']', '_').replace('<', '_') for name in feature_names]
-
     X_Dm = xgb.DMatrix(X, feature_names=feature_names)
     y_pred = model.predict(X_Dm)
-
     readmitted = y_pred >= 0.5
-
     result = {
         'readmitted_probability': float(y_pred[0]),
         'readmitted': bool(readmitted[0])
     }
     return result
+
+@app.post('/Group')
+async def upload_and_predict(file: UploadFile):
+    if file.filename.endswith('.csv'):
+        contents = await file.read()
+        input_df = pd.read_csv(io.StringIO(contents.decode('utf-8')))
+        X = preprocessor.transform(input_df)
+        feature_names = preprocessor.get_feature_names_out()
+        feature_names = [name.replace('[', '_').replace(']', '_').replace('<', '_') for name in feature_names]
+        X_Dm = xgb.DMatrix(X, feature_names=feature_names)
+        y_pred = model.predict(X_Dm)
+        readmitted = y_pred >= 0.5
+
+        # Convert float32 values to float
+        y_pred = [float(val) for val in y_pred]
+
+        # Convert bool values to int (1 for True, 0 for False)
+        readmitted = [int(val) for val in readmitted]
+
+        # Calculate the proportion of positive values
+        proportion_positive = sum(readmitted) / len(readmitted)
+
+        result = {
+            'readmitted_probability': y_pred,
+            'readmitted': readmitted,
+            'proportion_positive': proportion_positive
+        }
+        return JSONResponse(content=result)
+    else:
+        raise HTTPException(status_code=400, detail="File must be a CSV")
+
