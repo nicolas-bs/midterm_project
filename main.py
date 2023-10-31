bf75ad6a06177390d368ec9c4f07add5a10a55b6
nicolas-bs <69317512+nicolas-bs@users.noreply.github.com>
2023-10-31 08:09:52 -0500

    Tested Run Docker Image O.K

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pickle
 import pandas as pd
-import xgboost as xgb
 from fastapi import FastAPI, HTTPException
+import xgboost as xgb
 from pydantic import BaseModel
 
 app = FastAPI()
+
 model_file = 'xgb_eta01.bin'
+
+with open(model_file, 'rb') as f_in:
+    preprocessor, model = pickle.load(f_in)
 
 class InputData(BaseModel):
     age: str
@@ -25,41 +29,23 @@ class InputData(BaseModel):
     change: str
     diabetes_med: str
 
-# Load the preprocessor and model
-with open(model_file, 'rb') as f_in:
-    preprocessor, model = pickle.load(f_in)
-
 @app.post('/predict')
 async def predict(data: InputData):
-    try:
-        # Convert the input data to a Pandas DataFrame
-        input_data = pd.DataFrame([data.dict()])
 
-        # Transform the input data using the preprocessor
-        X = preprocessor.transform(input_data)
+    input_data = [data.dict()]
+    input_df = pd.DataFrame(input_data)
 
-        # Get feature names
-        feature_names = preprocessor.get_feature_names_out()
-        feature_names = [name.replace('[', '_').replace(']', '_').replace('<', '_') for name in feature_names]
+    X = preprocessor.transform(input_df)
+    feature_names = preprocessor.get_feature_names_out()
+    feature_names = [name.replace('[', '_').replace(']', '_').replace('<', '_') for name in feature_names]
 
-        # Create a DMatrix
-        X_Dm = xgb.DMatrix(X, feature_names=feature_names)
+    X_Dm = xgb.DMatrix(X, feature_names=feature_names)
+    y_pred = model.predict(X_Dm)
 
-        # Make predictions
-        y_pred = model.predict(X_Dm)
+    readmitted = y_pred >= 0.5
 
-        # Check if the prediction is above a certain threshold (e.g., 0.5)
-        readmitted = y_pred >= 0.5
-
-        result = {
-            'readmitted_probability': float(y_pred[0]),
-            'readmitted': bool(readmitted[0])
-        }
-        return result
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5049)
+    result = {
+        'readmitted_probability': float(y_pred[0]),
+        'readmitted': bool(readmitted[0])
+    }
+    return result
---
+++ train.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import xgboost as xgb
+import pickle
+import numpy as np
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.metrics import roc_auc_score
+
+# Load the dataset
+df = pd.read_csv(r'hospital_readmissions.csv')
+
+df['readmitted'] = df['readmitted'].apply(lambda x: x == 'yes')
+
+for col in df.select_dtypes(include='object'):
+    df[col] = df[col].astype('category')
+
+df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+df_train, df_val = train_test_split(df_full_train, test_size=0.25, random_state=42)
+
+# Reset the indices of the dataframes
+df_train = df_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+# Separate the target variable
+y_train = df_train['readmitted'].values
+y_val = df_val['readmitted'].values
+y_test = df_test['readmitted'].values
+
+# Drop the target variable from the data
+df_train = df_train.drop(['readmitted'], axis=1)
+df_val = df_val.drop(['readmitted'], axis=1)
+df_test = df_test.drop(['readmitted'], axis=1)
+
+categorical_cols = ['glucose_test', 'A1Ctest']
+ordinal_cols = ['age', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3', 'change', 'diabetes_med']
+
+# Create a column transformer for preprocessing
+preprocessor = make_column_transformer(
+    (OneHotEncoder(handle_unknown='ignore', sparse_output=False), categorical_cols),
+    (OrdinalEncoder(), ordinal_cols),
+    remainder='passthrough'
+)
+
+X_train = preprocessor.fit_transform(df_train)
+X_val = preprocessor.transform(df_val)
+
+feature_names = preprocessor.get_feature_names_out()
+feature_names = [name.replace('[', '_').replace(']', '_').replace('<', '_') for name in feature_names]
+
+dtrain = xgb.DMatrix(X_train, label=y_train, feature_names=feature_names)
+dval = xgb.DMatrix(X_val, label=y_val, feature_names=feature_names)
+
+params = {
+    'eta': 0.1,
+    'max_depth': 4,
+    'min_child_weight': 5,
+    'objective': 'binary:logistic',
+    'seed': 1,
+    'verbosity': 2,
+    'gamma': 1,
+    'eval_metric': 'auc'
+}
+
+# Train the XGBoost model
+num_boost_round = 105  # Define the number of boosting rounds
+model = xgb.train(params, dtrain, num_boost_round=num_boost_round)
+
+# Predict using the trained model
+y_pred = model.predict(dval)
+print(roc_auc_score(y_val, y_pred).round(4))
+
+# Define a train function for later use
+def train(X, y, params, num_boost_round, feature_names):
+    X_transformed = preprocessor.transform(X)
+    dtrain = xgb.DMatrix(X_transformed, label=y, feature_names=feature_names)
+    model = xgb.train(params, dtrain, num_boost_round=num_boost_round)
+    return model
+
+# Define a predict function for later use
+def predict(X, preprocessor, model, feature_names):
+    X_transformed = preprocessor.transform(X)
+    X_Dmat = xgb.DMatrix(X_transformed, feature_names=feature_names)
+    y_pred = model.predict(X_Dmat)
+    return y_pred
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=1)
+scores = []
+
+for fold, (train_idx, val_idx) in enumerate(kfold.split(df_full_train)):
+    df_train = df_full_train.iloc[train_idx]
+    df_val = df_full_train.iloc[val_idx]
+    y_train = df_train['readmitted'].values
+    y_val = df_val['readmitted'].values
+    model = train(df_train, y_train, params, 105, feature_names)
+    y_pred = predict(df_val, preprocessor, model, feature_names)
+    auc = roc_auc_score(y_val, y_pred)
+    scores.append(auc)
+    print(f'AUC on fold {fold+1} is {auc}')
+
+print('Validation results:')
+print(f'Mean AUC: {np.mean(scores):.3f} +/- {np.std(scores):.3f}')
+
+model = train(df_full_train, df_full_train['readmitted'].values, params, num_boost_round, feature_names)
+y_pred = predict(df_test, preprocessor, model, feature_names)
+auc = roc_auc_score(y_test, y_pred)
+print(f'Final model AUC on the test set: {auc:.3f}')
+
+# Saving the Model
+output_file =  'xgb_eta01.bin'
+with open(output_file, 'wb') as f_out:
+    pickle.dump((preprocessor, model), f_out)
+
+print(f'The model is saved to {output_file}')
